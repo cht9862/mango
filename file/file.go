1c467bc2269ab94b110d4726f6c613e46185f4b1
cht9862 <57534914+cht9862@users.noreply.github.com>
2021-04-19 16:45:39 +0800

    Update file.go

--- file/file.go
+++ file/file.go
@@ -7,59 +7,50 @@ import (
 	"os"
 )
 
-
-
-func Fcreater(f string) *os.File {
+// 创建文件函数，返回两个值一个 文件句柄，一个error 错误
+func Fcreater(f string) (*os.File, error) {
 	f1, err := os.Create(f)
 	if err != nil {
 		fmt.Println("this crete err now")
+		return err
 	}
-	fmt.Println("this creted now")
 	return f1
 }
-
-func Fopener(f string) *os.File {
+// 打开文件函数，返回两个值一个 文件句柄，一个error 错误
+func Fopener(f string) (*os.File, error) {
 	f1, err := os.Open(f)
 	if err != nil {
 		fmt.Println("this crete err now")
+		return err
 	}
-	fmt.Println("this Open now")
 	return f1
 }
 
-func Freader(r *os.File) {
+// 读取文件函数,返回一个 string 类型的值
+func Freader(r *os.File) (string, bool) {
 	bf := bufio.NewReader(r)
 	for {
 		data, err := bf.ReadString('\n')
 		if err == io.EOF {
-			fmt.Println("读取完成")
+			return "", true
 		} else if err != nil {
 			fmt.Println("this reader err now")
-			return
+			return false
 		}
-		fmt.Println(data)
-		return
+		return data
 	}
 
 }
 
-func Fwriter(w *os.File, str string) {
+// 写入文件函数，返回一个bool值，通过bool判断成没成功
+func Fwriter(w *os.File, str string) bool{
 	n, err := w.Write([]byte(str))
 	if err == io.EOF {
-		fmt.Println("写入完成")
+		// fmt.Println("file writed OK")
+		return true
 	} else if err != nil {
 		fmt.Println("this writer err now")
-		return
+		return false
 	}
-	fmt.Printf("写入字节数：%d，写入完成\n", n)
 }
 
-
-// 下面是使用指导
-
-// func main() {
-// 	filename := ".\\a.txt"
-// 	f := Fcreater(filename)
-// 	Fwriter(f,"my name is zhangsan")
-// 	f.Close()
-// }
