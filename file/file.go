7254d260c404b85b7ed96f08262a9161e2a1fb48
cht9862 <57534914+cht9862@users.noreply.github.com>
2021-04-19 14:51:24 +0800

    Update file.go

--- file/file.go
+++ file/file.go
@@ -8,11 +8,11 @@ import (
 )
 
 
-type file struct {
+type File struct {
 	Filename string `json:"filename"`
 }
 
-func (f *file) Fcreater() *os.File {
+func (f *File) Fcreater() *os.File {
 	f1, err := os.Create(f.Filename)
 	if err != nil {
 		fmt.Println("this crete err now")
@@ -21,7 +21,7 @@ func (f *file) Fcreater() *os.File {
 	return f1
 }
 
-func (f *file) Fopener() *os.File {
+func (f *File) Fopener() *os.File {
 	f1, err := os.Open(f.Filename)
 	if err != nil {
 		fmt.Println("this crete err now")
@@ -30,7 +30,7 @@ func (f *file) Fopener() *os.File {
 	return f1
 }
 
-func (f *file) Freader(r *os.File) {
+func (f *File) Freader(r *os.File) {
 	bf := bufio.NewReader(r)
 	for {
 		data, err := bf.ReadString('\n')
@@ -46,7 +46,7 @@ func (f *file) Freader(r *os.File) {
 
 }
 
-func (f *file) Fwriter(w *os.File, str string) {
+func (f *File) Fwriter(w *os.File, str string) {
 	n, err := w.Write([]byte(str))
 	if err == io.EOF {
 		fmt.Println("写入完成")
@@ -57,12 +57,15 @@ func (f *file) Fwriter(w *os.File, str string) {
 	fmt.Printf("写入字节数：%d，写入完成\n", n)
 }
 
-func main() {
-	var myFile file = file{Filename: ".\\a.txt"}
-	f := myFile.Fcreater()
-	defer f.Close()
-	myFile.Fwriter(f, "we come to 中国\n")
-	f = myFile.Fopener()
-	myFile.Freader(f)
 
-}
+// 下面是使用指导
+
+// func main() {
+// 	var myFile file = file{Filename: ".\\a.txt"}
+// 	f := myFile.Fcreater()
+// 	defer f.Close()
+// 	myFile.Fwriter(f, "we come to 中国\n")
+// 	f = myFile.Fopener()
+// 	myFile.Freader(f)
+
+// }
